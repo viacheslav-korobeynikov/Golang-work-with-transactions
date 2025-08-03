0945f17868cdde997991c2bc9cc3369728cc97d6
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-08-03 07:54:00 +0300

    Добавление транзакций в массив

--- main.go
+++ main.go
@@ -2,7 +2,39 @@ package main
 
 import "fmt"
 
+// В цикле спрашиваем ввод транзакций
+// Добавлять каждую введенную транзакцию в массив
+// Вывести массив
+
 func main() {
-	transactions := [3]int{5, 10, -7}
-	fmt.Println(transactions)
+	transactions := []int{}
+	for {
+		transactions = append(transactions, getUserInput())
+		isRepeatInput := checkUserChoice()
+		fmt.Println(transactions)
+		if !isRepeatInput {
+			break
+		} else {
+			continue
+		}
+	}
+
+}
+
+func getUserInput() int {
+	var transaction int
+	fmt.Println("Введите значение транзакции: ")
+	fmt.Scan(&transaction)
+	return transaction
+}
+
+func checkUserChoice() bool {
+	var userChoice string
+	fmt.Println("Вы хотите добавить еще транзакцию (y/n): ")
+	fmt.Scan(&userChoice)
+	if userChoice == "y" || userChoice == "Y" {
+		return true
+	} else {
+		return false
+	}
 }
